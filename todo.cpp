bfd81370e6647189a7419047c7bc5f21a2f100fa
Bine Stancic <39832894+BineStancic@users.noreply.github.com>
2019-07-18 17:24:20 +0200

    Update todo.cpp

--- todo.cpp
+++ todo.cpp
@@ -1,5 +1,8 @@
 #include<iostream>
 #include<string>
+#include<stdio.h>
+
+
 
 class Todolist {
 private:
@@ -10,75 +13,97 @@ private:
 
 public:
 	void read_data();
+	void show_data();
+	void input_entry();
+	void show_entry();
+	void check_entry();
+	void delete_entry();
 
 
-	//void add_title(std::string new_title);
-	//std::string get_title();
-
-	//void add_artist(std::string new_artist);
-	//std::string get_artist();
 
 };
 
-// add Song method definitions here:
 void Todolist::read_data() {
 	std::cout << "Task number: ";
 	std::cin >> number;
 	std::cout << "\nWhat needs to be done? ";
 	std::cin >> todo;
-	std::cout << "When does it need to be done by? ";
+	std::cout << "\nWhen does it need to be done by? ";
 	std::cin >> dateby;
-	std::cout << "Is it done? ";
+	std::cout << "\nIs it done? ";
 	std::cin >> finished;
-	//title = new_title;
+	std::cout << std::endl;
 }
 
 
 void Todolist::show_data() {
-	std::cout << "Task number: " << number << endl;
-	std::cout << "To do: " << todo << endl;
-	std::cout << "Deadline: " << dateby << endl;
-	std::cout<< "Progre"
+	std::cout << "Task number: " << number << std::endl;
+	std::cout << "To do: " << todo << std::endl;
+	std::cout << "Deadline: " << dateby << std::endl;
+	std::cout << "Progress: " << finished << std::endl;
+	std::cout << "___________________________" << std::endl;
+}
+
+void Todolist::input_entry() {
+	read_data();
+}
+
+void Todolist::show_entry() {
+	show_data();
+}
+
+void Todolist::check_entry() {
+	bool done;
+	std::cout << "\n There progress of object " << finished;
+	std::cout << "\n Enter the new progress: ";
+	std::cin >> finished;
+	show_entry();
 
 }
 
-void Todolist::edit_data() {
+void Todolist::delete_entry() {
+	int n;
+	std::cout << "\n Enter task to Delete: ";
+	std::cin >> n;
 
 
 }
 
-
-void Todolist::
-
-
-
-
-
-std::string Song::get_title() {
-
-  return title;
-
-}
-
-void Song::add_artist(std::string new_artist) {
-
-  artist = new_artist;
-
-}
-
-std::string Song::get_artist() {
-
-  return artist;
-
-}
 
 int main() {
+	Todolist clas;
+	int number;
+	while (true) {
+		std::cout << "\n\t1-->Add a thing that needs to be done";
+		std::cout << "\n\t2-->Show what needs to be done ";
+		std::cout << "\n\t3-->Check something off ";
+		std::cout << "\n\t4-->Delete a entry";
+		std::cout << "\nEnter your choice: ";
+		std::cin >> number;
 
-	Song electric_relaxation;
+		switch (number) {
+		case 1:
+			clas.input_entry();
+			break;
+		
+		case 2:
+			clas.show_entry();
+			break;
 
-	electric_relaxation.add_artist("A Tribe Called Quest");
-	std::cout << electric_relaxation.get_artist();
+		case 3:
+			clas.check_entry();
+			break;
 
+		case 4:
+			clas.delete_entry();
+			break;
+
+		default:
+			std::cout << "\nEnter corret choice";
+			exit(0);
+		}
+	}
+
+	system("pause");
+	return 0;
 }
-
-
